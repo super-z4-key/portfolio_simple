22dcdb6e85dea75c48d1da45d7e7c9d96879b790
superrrzzz <ubaidillahzaky11@gmail.com>
2022-11-25 07:05:50 +0700

    merapikan script

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 const liNavBar = document.querySelectorAll(".li-navbar");
 const liBody = document.querySelectorAll(".li-body");
-
 const clickButton = (params) => {
   if (params == params) {
     for (let i = 0; i < liNavBar.length; i++) {
       if (i == params) {
         liNavBar[i].style.backgroundColor = "#d7aa92";
-      } else {
-        liNavBar[i].style.backgroundColor = "#e3cec6";
-      }
-    }
-    for (let i = 0; i < liBody.length; i++) {
-      if (i == params) {
         liBody[i].style.display = "flex";
       } else {
+        liNavBar[i].style.backgroundColor = "#e3cec6";
         liBody[i].style.display = "none";
       }
     }
