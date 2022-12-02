1b959b26ec2ee5f84bd3591e6fa2ac32d9b079b9
superrrzzz <ubaidillahzaky11@gmail.com>
2022-12-02 07:54:39 +0700

    Update Version 2.0.0

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const liNavBar = document.querySelectorAll(".li-navbar");
-const liBody = document.querySelectorAll(".li-body");
-const clickButton = (params) => {
-  if (params == params) {
-    for (let i = 0; i < liNavBar.length; i++) {
-      if (i == params) {
-        liNavBar[i].style.backgroundColor = "#d7aa92";
-        liBody[i].style.display = "flex";
-      } else {
-        liNavBar[i].style.backgroundColor = "#e3cec6";
-        liBody[i].style.display = "none";
-      }
-    }
-  }
-};
+// const liNavBar = document.querySelectorAll(".li-navbar");
+// const liBody = document.querySelectorAll(".li-body");
+// const clickButton = (params) => {
+//   if (params == params) {
+//     for (let i = 0; i < liNavBar.length; i++) {
+//       if (i == params) {
+//         liNavBar[i].style.backgroundColor = "gray";
+//         liNavBar[i].style.color = "white";
+//         liBody[i].style.display = "flex";
+//       } else {
+//         liNavBar[i].style.backgroundColor = "white";
+//         liNavBar[i].style.color = "black";
+//         liBody[i].style.display = "none";
+//       }
+//     }
+//   }
+// };
